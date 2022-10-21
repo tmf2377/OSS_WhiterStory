dfdd12524db979a914eae486406226afdbbfff76
kimgyeran <kgh08813@gmail.com>
2022-10-22 08:48:38 +0900

    전투&상점 구현 완료

--- OSS_WhiterStory/Assets/2Scripts/Weapon.cs
+++ OSS_WhiterStory/Assets/2Scripts/Weapon.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum Type
     {
-        
+        Melee,Range
+    };
+    public Type type;
+    public int damage;
+    public float rate;
+    public BoxCollider meleeArea;
+    public TrailRenderer trailEffect;
+    public Transform bulletPos;
+    public GameObject bullet;
+    public Transform bulletCasePos;
+    public GameObject bulletCase;
+    public int maxAmmo;
+    public int curAmmo;
+    public void Use()
+    {
+        if(type == Type.Melee)
+        {
+            StopCoroutine("Swing");
+            StartCoroutine("Swing");
+        }
+        else if (type == Type.Range&&curAmmo>0)
+        {
+            curAmmo--;
+            StartCoroutine("Shot");
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator Swing()
     {
-        
+        yield return new WaitForSeconds(0.1f);
+        meleeArea.enabled = true;
+        trailEffect.enabled = true;
+
+        yield return new WaitForSeconds(0.3f);
+        meleeArea.enabled = false;
+        yield return new WaitForSeconds(0.3f);
+        trailEffect.enabled = false;
+    }
+    IEnumerator Shot()
+    {
+        //#1 총알 발사
+        GameObject intantBullet = Instantiate(bullet,bulletPos.position,bulletPos.rotation);
+        Rigidbody bulletRigid = intantBullet.GetComponent<Rigidbody>();
+        bulletRigid.velocity = bulletPos.forward * 50;
+
+        yield return null;
+        //#2 탄피 배출
+        GameObject intantCase = Instantiate(bulletCase, bulletCasePos.position, bulletCasePos.rotation);
+        Rigidbody caseRigid = intantCase.GetComponent<Rigidbody>();
+        Vector3 caseVec = bulletCasePos.forward * Random.Range(-3, -2) + Vector3.up * Random.Range(2, 3);
+        caseRigid.AddForce(caseVec,ForceMode.Impulse);
+        caseRigid.AddTorque(Vector3.up * 10,ForceMode.Impulse);
     }
 }
