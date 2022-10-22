82a35fa37842abddd8bcde96443ec15693606fa4
JinYoungBae-kw <bjo0302@naver.com>
2022-10-22 19:21:09 +0900

    monster~AI

--- OSS_WhiterStory/Assets/2Scripts/Enemy.cs
+++ OSS_WhiterStory/Assets/2Scripts/Enemy.cs
@@ -1,21 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public int maxHealth;
     public int curHealth;
+    public Transform target;
+    public bool isChase;
 
     Rigidbody rigid;
     BoxCollider boxCollider;
     Material mat;
+    NavMeshAgent nav;
+    Animator anim;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         boxCollider = GetComponent<BoxCollider>();
-        mat = GetComponent<MeshRenderer>().material;
+        mat = GetComponentInChildren<MeshRenderer>().material;
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+
+        Invoke("ChaseStart", 2);
+    }
+
+    void ChaseStart()
+    {
+        isChase = true;
+        anim.SetBool("isWalk", true);
+    }
+    void Update()
+    {
+        if(isChase)
+            nav.SetDestination(target.position);
+    }
+    void FixedUpdate()
+    {
+        FreezeVelocity();
+    }
+
+    void FreezeVelocity()
+    {
+        if(isChase)
+        {
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = Vector3.zero;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -53,6 +86,10 @@ public class Enemy : MonoBehaviour
         {
             mat.color = Color.gray;
             gameObject.layer = 14;
+            isChase = false;
+            nav.enabled = false;
+            anim.SetTrigger("doDie");
+
             if (isGrenade)
             {
                 reactVec = reactVec.normalized;
