94d4653c3dc4c25bf50a432515537d92699ae881
seoa <tmf2377@naver.com>
2022-11-13 02:01:31 +0900

    인벤토리 구현중

---
+++ OSS_WhiterStory/Assets/2Scripts/Inventory/FieldItems.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldItems : MonoBehaviour
+{
+    public InventItem item;
+    //public SpriteRenderer image;
+    public GameObject potion;
+
+    public void SetItem(InventItem _item)
+    {
+        item.itemName = _item.itemName;
+        item.itemImage = _item.itemImage;
+        item.itemType = _item.itemType;
+        item.efts = _item.efts;
+
+        //image.sprite = item.itemImage;
+       
+    }
+
+    public InventItem GetItem()
+    {
+        return item;
+    }
+
+    public void DestroyItem()
+    {
+        Destroy(gameObject);
+    }
+}
