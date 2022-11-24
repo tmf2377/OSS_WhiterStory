f032391d9b704bb99056c0edddc9d7b0ebfbfa00
JinYoungBae-kw <bjo0302@naver.com>
2022-11-24 14:31:26 +0900

    first-commit

--- OSS_WhiterStory/Assets/2Scripts/BossNPC.cs
+++ OSS_WhiterStory/Assets/2Scripts/BossNPC.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class NPC : MonoBehaviour
+public class BossNPC : MonoBehaviour
 {
     public int CntBoss;
     public int KillBoss;
