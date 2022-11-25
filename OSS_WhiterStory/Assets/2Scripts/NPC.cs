1c286ece792328cad0c51616abc49631abe46085
JinYoungBae-kw <bjo0302@naver.com>
2022-11-25 18:48:57 +0900

    npc 대화수정중

--- OSS_WhiterStory/Assets/2Scripts/NPC.cs
+++ OSS_WhiterStory/Assets/2Scripts/NPC.cs
@@ -7,10 +7,15 @@ using UnityEngine.SceneManagement;
 public class NPC : MonoBehaviour
 {
     public RectTransform uiGroup;
+    public RectTransform uiGroup1;
+    public RectTransform uiGroup2;
+    public RectTransform uiGroup3;
     public Animator anim;
     public GameManager manager;
 
+
     Player enterPlayer;
+
     public void Enter(Player player) //입장
     {
         enterPlayer = player;
@@ -20,6 +25,27 @@ public class NPC : MonoBehaviour
     public void NpcExit() //일반 대화 퇴장
     {
         anim.SetTrigger("doHello");
-        uiGroup.anchoredPosition = Vector3.down * 2031;
+        uiGroup.anchoredPosition = Vector3.down * 2049;
+    }
+
+    public void NpcTalk()
+    {
+        uiGroup.anchoredPosition = Vector3.down * 2049;
+        uiGroup1.anchoredPosition = Vector3.zero;
+    }
+    public void NpcTalk1()
+    {
+        uiGroup1.anchoredPosition = Vector3.down * 2573;
+        uiGroup2.anchoredPosition = Vector3.zero;
+    }
+    public void NpcTalk2()
+    {
+        uiGroup2.anchoredPosition = Vector3.down * 3087;
+        uiGroup3.anchoredPosition = Vector3.zero;
+    }
+    public void GameStart()
+    {
+        anim.SetTrigger("doHello");
+        uiGroup3.anchoredPosition = Vector3.down * 3613;
     }
 }
