175294aa02745bec0ec1111781886942eedb1501
JinYoungBae-kw <bjo0302@naver.com>
2022-11-25 23:51:38 +0900

    스테이지2대화

--- OSS_WhiterStory/Assets/2Scripts/NPC.cs
+++ OSS_WhiterStory/Assets/2Scripts/NPC.cs
@@ -10,6 +10,9 @@ public class NPC : MonoBehaviour
     public RectTransform uiGroup1;
     public RectTransform uiGroup2;
     public RectTransform uiGroup3;
+    public RectTransform uiGroup4;
+    public RectTransform uiGroup5;
+    public Text startTalktext;
     public Animator anim;
     public GameManager manager;
 
@@ -25,6 +28,8 @@ public class NPC : MonoBehaviour
     public void Enter(Player player) //입장
     {
         enterPlayer = player;
+        if (manager.stage == 2)
+            startTalktext.text = "이게 누구야!! 흰둥이 아니야??";
         uiGroup.anchoredPosition = Vector3.zero;
         if (IsPause == false)
         {
@@ -39,14 +44,25 @@ public class NPC : MonoBehaviour
         uiGroup.anchoredPosition = Vector3.down * 2049;
         uiGroup1.anchoredPosition = Vector3.down * 2573;
         uiGroup2.anchoredPosition = Vector3.down * 3087;
+        uiGroup3.anchoredPosition = Vector3.down * 3613;
+        uiGroup4.anchoredPosition = Vector3.down * 6750;
+        uiGroup5.anchoredPosition = Vector3.down * 7275;
         Time.timeScale = 1;
         IsPause = false;
     }
 
     public void NpcTalk()
     {
-        uiGroup.anchoredPosition = Vector3.down * 2049;
-        uiGroup1.anchoredPosition = Vector3.zero;
+        if(manager.stage == 1)
+        {
+            uiGroup.anchoredPosition = Vector3.down * 2049;
+            uiGroup1.anchoredPosition = Vector3.zero;
+        }
+        else if(manager.stage == 2)
+        {
+            uiGroup.anchoredPosition = Vector3.down * 2049;
+            uiGroup4.anchoredPosition = Vector3.zero;
+        }
     }
     public void NpcTalk1()
     {
@@ -58,10 +74,18 @@ public class NPC : MonoBehaviour
         uiGroup2.anchoredPosition = Vector3.down * 3087;
         uiGroup3.anchoredPosition = Vector3.zero;
     }
+    public void NpcTalk3()
+    {
+        uiGroup4.anchoredPosition = Vector3.down * 6750;
+        uiGroup5.anchoredPosition = Vector3.zero;
+    }
     public void GameStart()
     {
         anim.SetTrigger("doHello");
-        uiGroup3.anchoredPosition = Vector3.down * 3613;
+        if(manager.stage == 1)
+            uiGroup3.anchoredPosition = Vector3.down * 3613;
+        else if(manager.stage == 2)
+            uiGroup5.anchoredPosition = Vector3.down * 7275;
         Time.timeScale = 1;
         IsPause = false;
     }
